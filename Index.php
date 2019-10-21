78bd4a8178d7e98b86186d890965e239dfcda93d
Marcanjelo Difoglio <33300902+RubyRose45@users.noreply.github.com>
2019-10-21 11:29:12 -0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/elements/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="colorlib">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Himiko Toga</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/nice-select.css">			
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>  
+			<!-- Start banner Area -->
+			<section class="generic-banner relative">
+			<!-- Start Header Area -->
+				 <?php include "Header.php";  ?>
+			<!-- End Header Area -->				
+				<div class="container">
+					<div class="row height align-items-center justify-content-center">
+						<div class="col-lg-10">
+							<div class="generic-banner-content">
+								<h2 class="text-white text-center">CIS-12 Blog</h2>
+								<p class="text-white">Project For The Class.</p>
+							</div>							
+						</div>
+					</div>
+				</div>
+			</section>		
+			<!-- End banner Area -->
+		
+		<!-- About Generic Start -->
+		<div class="main-wrapper">
+
+			<!-- Start fashion Area -->
+			<section class="fashion-area section-gap" id="fashion">
+				<div class="container">					
+					<div class="row">
+						<div class="col-lg-3 col-md-6 single-fashion">
+							<img class="img-fluid" src="img/f1.jpg" alt="">
+							<p class="date">10 Jan 2018</p>
+                                                        <h4><a href="Article.php">Addiction When Gambling
+							Becomes A Problem</a></h4>
+							<p>
+								inappropriate behavior ipsum dolor sit amet, consectetur.
+							</p>
+							<div class="meta-bottom d-flex justify-content-between">
+								<p><span class="lnr lnr-heart"></span> 15 Likes</p>
+								<p><span class="lnr lnr-bubble"></span> 02 Comments</p>
+							</div>									
+						</div>
+						<div class="col-lg-3 col-md-6 single-fashion">
+							<img class="img-fluid" src="img/f2.jpg" alt="">
+							<p class="date">10 Jan 2018</p>
+							<h4><a href=""Article.php"">Addiction When Gambling
+							Becomes A Problem</a></h4>
+							<p>
+								inappropriate behavior ipsum dolor sit amet, consectetur.
+							</p>
+							<div class="meta-bottom d-flex justify-content-between">
+								<p><span class="lnr lnr-heart"></span> 15 Likes</p>
+								<p><span class="lnr lnr-bubble"></span> 02 Comments</p>
+							</div>									
+						</div>
+						<div class="col-lg-3 col-md-6 single-fashion">
+							<img class="img-fluid" src="img/f3.jpg" alt="">
+							<p class="date">10 Jan 2018</p>
+							<h4><a href=""Article.php"">Addiction When Gambling
+							Becomes A Problem</a></h4>
+							<p>
+								inappropriate behavior ipsum dolor sit amet, consectetur.
+							</p>
+							<div class="meta-bottom d-flex justify-content-between">
+								<p><span class="lnr lnr-heart"></span> 15 Likes</p>
+								<p><span class="lnr lnr-bubble"></span> 02 Comments</p>
+							</div>									
+						</div>
+						<div class="col-lg-3 col-md-6 single-fashion">
+							<img class="img-fluid" src="img/f4.jpg" alt="">
+							<p class="date">10 Jan 2018</p>
+							<h4><a href=""Article.php"">Addiction When Gambling
+							Becomes A Problem</a></h4>
+							<p>
+								inappropriate behavior ipsum dolor sit amet, consectetur.
+							</p>
+							<div class="meta-bottom d-flex justify-content-between">
+								<p><span class="lnr lnr-heart"></span> 15 Likes</p>
+								<p><span class="lnr lnr-bubble"></span> 02 Comments</p>
+							</div>									
+						</div>						
+					</div>
+				</div>	
+			</section>
+			<!-- End fashion Area -->			
+
+			<!-- Start Generic Area -->
+			<section class="about-generic-area pb-100">
+				<div class="container border-top-generic">
+					<h3 class="about-title mb-30">Elaboration about Generic Page</h3>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="img-text">
+								<img src="img/a.jpg" alt="" class="img-fluid float-left mr-20 mb-20">
+								<p>Recently, the US Federal government banned online casinos from operating in America by making it illegal to transfer money to them through any US bank or payment system. As a result of this law, most of the popular online casino networks such as Party Gaming and PlayTech left the United States. Overnight, online casino players found themselves being chased by the Federal government. But, after a fortnight, the online casino industry came up with a solution and new online casinos started taking root. These began to operate under a different business umbrella, and by doing that, rendered the transfer of money to and from them legal. A major part of this was enlisting electronic banking systems that would accept this new clarification and start doing business with me. Listed in this article are the electronic banking systems that accept players from the United States that wish to play in online casinos.</p>						 
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End Generic Start -->		
+
+			<!-- start footer Area -->		
+			 <?php include "Footer.php";  ?>
+			<!-- End footer Area -->		
+
+		</div>
+		<script src="js/vendor/jquery-2.2.4.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+		<script src="js/vendor/bootstrap.min.js"></script>
+		<script src="js/jquery.ajaxchimp.min.js"></script>
+		<script src="js/jquery.sticky.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.nice-select.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/main.js"></script>
+	</body>
+</html>
---
+++ Profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <link href="Assets/Style.css" rel="stylesheet" type="text/css"/>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body> 
+        <div class="topnav">         
+            <a href="Article.php">Article</a>
+            <a href="Articles.php">Articles</a>
+            <a href="Index.php">Index</a>
+            <a href="Login.php">Login</a>
+            <a class="active" href="#">Profile</a>          
+            <a href="Register.php">Register</a>
+        </div>
+        
+        <h1>Index</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
